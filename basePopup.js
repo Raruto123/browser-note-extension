f1d12ea625fcf7df1f59227a83da43f9b0ff8544
mohamedsane <sanem9206@gmail.com>
2025-02-26 19:17:34 +0000

    update the app
    trying to identify each note to remove them independently

--- basePopup.js
+++ basePopup.js
@@ -23,10 +23,35 @@ function loadAndShowNotes() {
     const notes = data.notes || {}; // Assurer que notes existe
 
     console.log(`%c🎨 ⍨ Notes chargées`, "color:yellowgreen; font-weight:bold", notes);
-
+    document.getElementById("delete-all-notes").addEventListener("click", () => {
+      for(const url in notes) {
+        delete notes[url];
+      }
+      console.log(`%c🎨 ⍨ notes`, "color:yellow; font-weight:bold", notes);
+    })
     for (const url in notes) {
       const element = document.createElement("li");
       element.textContent = `${url} : ${notes[url]}`;
+
+      //bouton supprimer
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Supprimer";
+      deleteButton.type = "button";
+      siteList.childNodes.forEach(() => {
+        if(siteList.childNodes.length === 0) {
+          deleteButton.dataset.order = 0
+        } else {
+          deleteButton.dataset.order = siteList.childNodes.length;
+        };
+      });
+      deleteButton.addEventListener("click", () => {
+        console.log(siteList.childNodes);
+      })
+
+      //je colle le bouton supprimer à la list element
+      element.appendChild(deleteButton);
+
+      //je colle le element au siteList
       siteList.appendChild(element);
     }
   });
@@ -53,3 +78,7 @@ document.getElementById("save-note").addEventListener("click", async () => {
     alert("Insérez des valeurs valides!");
   }
 });
+
+function eraseNote() {
+  console.log("lel")
+}
